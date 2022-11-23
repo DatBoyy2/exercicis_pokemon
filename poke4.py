e4312eb72ea1bc88bf1ad002399fb86a23cb72aa
DatBoyy2 <guilleme67@gmail.com>
2022-11-23 08:18:48 +0100

    Update poke4.py

--- poke4.py
+++ poke4.py
@@ -2,5 +2,4 @@ n=int(input("A que nivel quieres que llegue tu pokemon?"))
 
 e=(0.8*(n**3))
 
-print("la experiencia necesaria para que tu pokemon evolucione es:")
-print(e)
+print(f'la experiencia necesaria para que tu pokemon suba a nivel {n} es {e} xp')
